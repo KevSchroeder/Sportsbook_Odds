72acdc63327f49793b039189708d34ce25aba5fd
Kevin Schroeder <95885765+KevSchroeder@users.noreply.github.com>
2024-02-21 22:09:05 -0500

    aligned classes with files

--- db.py
+++ db.py
@@ -2,10 +2,16 @@ from sqlalchemy import create_engine, ForeignKey, Column, String, Integer, CHAR
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
+#DB connection
+DB_URI = #postgreSQL credentials
+engine = create_engine(DB_URI)
 Base = declarative_base()
+Session = sessionmaker(bind=engine)
+session = Session()
 
-class Person(Base):
-    __tablename__ = 'odds'
+
+class Prop(Base):
+    __tablename__ = 'props'
 
     name = Column("name", String, primary_key=True)
     points = Column("points", Integer)
@@ -20,10 +26,20 @@ class Person(Base):
         return f"({self.name} {self.points} {self.prop})"
     
 
-engine = create_engine("sqlite:///mydb.db", echo=True)
-Base.metadata.create_all(bind=engine)
+#Insert data into database
+def insert_props_into_database():
+    for prop_title, prop_description in scrape_draftkings_by_category():
+        prop = Prop(prop_title, prop_description)
+        session.add(prop)
+    session.commit()
 
-Session = sessionmaker(bind=engine)
-session = Session()
+#Create DB tables
+Base.metadata.create_all(engine)
+
+#Insert scraped data into database
+insert_props_into_database()
+
+
+
 
 person = Person(1231, "Mike", "Smith", "m", 25)
